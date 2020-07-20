410851a0515746b241a63f003c8b24e6e6b26901
Rikiborn <64733239+Rikiborn@users.noreply.github.com>
2020-07-20 18:22:56 +0300

    Update death.lua

--- disc-death/server/death.lua
+++ disc-death/server/death.lua
@@ -1,28 +1,28 @@
 RegisterServerEvent('disc-death:setDead')
 AddEventHandler('disc-death:setDead', function(isDead)
-    local xPlayer = ESX.GetPlayerFromId(source)
+    local player = ESX.GetPlayerFromId(source)
     
     MySQL.Async.execute('UPDATE users SET is_dead = @isDead WHERE identifier = @identifier', {
         ['@isDead'] = isDead,
-        ['@identifier'] = xPlayer.getIdentifier()
+        ['@identifier'] = player.getIdentifier()
     })
 end)
 
 ESX.RegisterServerCallback('disc-death:getDead', function(source, cb)
-    local xPlayer = ESX.GetPlayerFromId(source)
+    local player = ESX.GetPlayerFromId(source)
 
     MySQL.Async.fetchAll('SELECT is_dead FROM users WHERE identifier = @identifier', {
-        ['@identifier'] = xPlayer.getIdentifier()
+        ['@identifier'] = player.getIdentifier()
     }, function(results)
         cb(results[1].is_dead)
     end)
 end)
 
 ESX.RegisterServerCallback('disc-death:isPlayerDead', function(source, cb, target)
-    local xPlayer = ESX.GetPlayerFromId(target)
+    local player = ESX.GetPlayerFromId(target)
 
     MySQL.Async.fetchAll('SELECT is_dead FROM users WHERE identifier = @identifier', {
-        ['@identifier'] = xPlayer.getIdentifier()
+        ['@identifier'] = player.getIdentifier()
     }, function(results)
         cb(results[1].is_dead)
     end)
